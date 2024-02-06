a2301f025d4a6f1f3226f4072fca8a3649d6d95a
Okeskar <60957051+Okeskar@users.noreply.github.com>
2024-02-06 21:04:47 +0530

    Update DGPS_Points_3_style.js

--- styles/DGPS_Points_3_style.js
+++ styles/DGPS_Points_3_style.js
@@ -9,7 +9,7 @@ var style_DGPS_Points_3 = function(feature, resolution){
     var value = ""
     var labelText = "";
     size = 0;
-    var labelFont = "20px \'Arial\', sans-serif";
+    var labelFont = "22px \'Arial\', sans-serif";
     var labelFill = "#000000";
     var bufferColor = "";
     var bufferWidth = 0;
@@ -22,7 +22,7 @@ var style_DGPS_Points_3 = function(feature, resolution){
     }
     var style = [ new ol.style.Style({
         image: new ol.style.Circle({radius: 4.0 + size,
-            stroke: new ol.style.Stroke({color: 'rgba(255,0,0,1.0)', lineDash: null, lineCap: 'butt', lineJoin: 'miter', width: 0.76}), fill: new ol.style.Fill({color: 'rgba(0,0,0,1.0)'})}),
+            stroke: new ol.style.Stroke({color: 'rgba(0,0,0,1.0)', lineDash: null, lineCap: 'butt', lineJoin: 'miter', width: 0.76}), fill: new ol.style.Fill({color: 'rgba(0,0,0,1.0)'})}),
         text: createTextStyle(feature, resolution, labelText, labelFont,
                               labelFill, placement, bufferColor,
                               bufferWidth)
