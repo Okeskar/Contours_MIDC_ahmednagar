a077a80733d5a6e573dfe10f48a5e355e59237ca
Okeskar <60957051+Okeskar@users.noreply.github.com>
2024-02-06 21:05:43 +0530

    Update Buffe_Only_2_style.js

--- styles/Buffe_Only_2_style.js
+++ styles/Buffe_Only_2_style.js
@@ -21,7 +21,7 @@ var style_Buffe_Only_2 = function(feature, resolution){
         labelText = String("");
     }
     var style = [ new ol.style.Style({
-        stroke: new ol.style.Stroke({color: 'rgba(255,0,0,1.0)', lineDash: null, lineCap: 'square', lineJoin: 'bevel', width: 5}),
+        stroke: new ol.style.Stroke({color: 'rgba(0,0,0,1.0)', lineDash: null, lineCap: 'square', lineJoin: 'bevel', width: 5}),
         text: createTextStyle(feature, resolution, labelText, labelFont,
                               labelFill, placement, bufferColor,
                               bufferWidth)
